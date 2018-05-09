576aa995bf60a6cb06b1ed1adaeb3bcf7189f9e0
risonakamo <hmykanae@gmail.com>
2018-05-09 13:34:44 -0400

    col box expansion

--- index.js
+++ index.js
@@ -2,5 +2,32 @@ window.onload=main;
 
 function main()
 {
+    colboxevents();
+}
 
+//col-box outside event handlers
+function colboxevents()
+{
+    var colboxs=document.querySelectorAll("col-box");
+    var currentOpen;
+
+    for (var x=0;x<colboxs.length;x++)
+    {
+        colboxs[x].addEventListener("expanded",(e)=>{
+            if (currentOpen)
+            {
+                if (currentOpen==e.currentTarget)
+                {
+                    currentOpen=null;
+                }
+
+                else
+                {
+                    currentOpen.classList.remove("expanded");
+                }
+            }
+
+            currentOpen=e.currentTarget;
+        });
+    }
 }
